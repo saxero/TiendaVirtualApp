ca89ee8d918c897f3308b2a6bf02a0234e8b62a2
saxero <jvives@gmail.com>
2017-12-03 16:38:39 +0100

    Xamarin Blank Solution

---
+++ TiendaVirtualApp/TiendaVirtualApp/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TiendaVirtualApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
