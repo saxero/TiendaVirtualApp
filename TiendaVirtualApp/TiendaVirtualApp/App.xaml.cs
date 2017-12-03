1cd8634d9c4212172180eb72d3789989389f2fee
saxero <jvives@gmail.com>
2017-12-03 17:52:46 +0100

    MasterDetail + Pages + Icons

--- TiendaVirtualApp/TiendaVirtualApp/App.xaml.cs
+++ TiendaVirtualApp/TiendaVirtualApp/App.xaml.cs
@@ -4,16 +4,20 @@ using System.Linq;
 using System.Text;
 
 using Xamarin.Forms;
+using TiendaVirtualApp.Pages;
 
 namespace TiendaVirtualApp
 {
     public partial class App : Application
     {
+        public static NavigationPage Navigator { get; internal set; }
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new TiendaVirtualApp.MainPage();
+            //MainPage = new TiendaVirtualApp.MainPage();
+            MainPage = new MasterPage();
         }
 
         protected override void OnStart()
